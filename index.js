972a8d9dd648a8d9541e5551d1be95b3c8484afc
pafz <paferza@gmail.com>
2023-07-28 18:37:05 +0200

    cors added ()

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { typeError } = require('./middleware/errors');
 const app = express();
 const PORT = 3000;
 const cors = require('cors');
-app.use(cors);
+app.use(cors());
 
 app.use(express.json());
 
